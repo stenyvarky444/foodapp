bfe2c861f36745d3ab3e3851151c25c09cc58ba1
stenyvarky444 <stenyvarky444@gmail.com>
2025-03-02 11:46:41 +0530

    getmapping

--- src/main/java/com/foodapp/controller/FoodController.java
+++ src/main/java/com/foodapp/controller/FoodController.java
@@ -2,9 +2,18 @@ package com.foodapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
+	
+	@GetMapping("/getdata")
+	public String getFoodData(@RequestParam String id) {
+		return new String();
+	}
+	
 
 }
